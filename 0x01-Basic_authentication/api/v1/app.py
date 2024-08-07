f7ecb928cec1dc906f0a109b1838d8f2b8e2042b
Youssef Rhaimouni <youssefgf123@gmail.com>
2024-08-07 08:37:08 -0700

    Update

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -26,3 +26,10 @@ if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
     app.run(host=host, port=port)
+
+@app.route('/api/v1/status')
+def status():
+    return "Service is running"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
